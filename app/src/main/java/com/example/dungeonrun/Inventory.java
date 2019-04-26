bed275d968f364954b5c1a756c97304003b1d0e0
Jacob <Whythough@gmail.com>
2019-04-25 20:29:38 -0400

    For Fun

---
+++ app/src/main/java/com/example/dungeonrun/Inventory.java
@@ -0,0 +1,41 @@
+package com.example.dungeonrun;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class Inventory extends AppCompatActivity  {
+    Player player;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        player = setPlayer();
+
+    }
+    public Player setPlayer()
+    {
+
+
+        Player player1 = new Player(1, 1, 0, 100);
+        Intent intent = getIntent();
+        if(intent != null) {
+            Bundle extras = getIntent().getExtras();
+
+
+            if (extras != null) {
+
+                player1 = (Player)intent.getSerializableExtra("P");
+
+            } else if (extras == null) {
+                System.out.println("Null");
+
+            }
+        }
+        System.out.println(intent.toString());
+        return player1;
+
+
+    }
+}
--- app/src/main/java/com/example/dungeonrun/levelUp.java
+++ app/src/main/java/com/example/dungeonrun/levelUp.java
@@ -1,15 +1,12 @@
 package com.example.dungeonrun;
 
-import android.app.ProgressDialog;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-
 
 public class levelUp  extends AppCompatActivity {
 
